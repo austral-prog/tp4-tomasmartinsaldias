8ba9005fb25d33f02a8ad5010b0c9f478fddfb4d
tomasmartinsaldias <tomasmartin.saldias@gmail.com>
2025-09-01 11:59:59 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
 def leap_year():
-    print("TO DO")
+    año = int(input("Ingrese un año: "))
+    if (año % 100 == 0 and año %400 == 0) or (año % 4 == 0 and año % 100 !=0):
+        print(f"El año {año} es bisiesto")
+    else:
+        print(f"El año {año} no es bisiesto")
+leap_year()
